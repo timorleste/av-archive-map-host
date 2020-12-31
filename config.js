579ec83e7f65df72700185842a48b5a39c06c7c7
ict-gis-mapping repositories project, design and prototype <24618720+timorleste@users.noreply.github.com>
2020-12-31 18:32:22 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,438 @@
+var config = {
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    accessToken: 'pk.eyJ1IjoidG9ueWJlaGFyIiwiYSI6ImNrajlya2RoZzFoZmkycXA5Nnpjc3BheGEifQ.JnEUpxQ2NVC9uumiPZ8JWA',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    theme: 'light',
+    use3dTerrain: false,
+    title: 'CAMSTL',
+    subtitle: 'Centro Audiovisual Max Stahl Timor Leste',
+    byline: 'The importance of The Archive derives from the crucial role that audiovisual material played in the East Timorese struggle for self-determination and human rights. That struggle was captured in the faces and the voices of this very dedicated, resilient and persistent people by International film-maker Max Stahl on over 3000 hours of film and audio recording.',
+    footer: '&copy; 2020  [ Rua Formosa No.19. Vera Cruz, Suco Colmera, Rai Nain. Dili, Timor-Leste ]',
+        
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Server Back Office Host Specification',
+            image: 'images/camstl.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+            
+            location: {
+                center: [125.57726, -8.55524],  
+                zoom: 18.00,
+                pitch: 19.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            
+            onChapterEnter: [
+               
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: '1other-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Server Front Office Host Specification [Partner]',
+            image: 'images/uc-pt.png',
+            description: 'The University of Coimbra, is a Portuguese public University in Coimbra Portugal, being one of the oldest universities in continuous operation in the world. On 22 June 2013, UNESCO added the university to its World Heritage List.',
+            
+            
+            location: {
+                center: [ -8.425943, 40.207225],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        {
+            id: 'AILEU',
+            alignment: 'left',
+            hidden: false,
+            title: 'Distrito AILEU [av_attachment_soon]',
+            image: 'images/AILEU.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+
+            location: {
+                center: [ 125.56693, -8.71643],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+        
+        
+        
+        
+        
+        
+         
+        {
+            id: 'AINARO',
+            alignment: 'right',
+            hidden: false,
+            title: 'Distrito AINARO [av_attachment_soon]',
+            image: 'images/AINARO.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+
+            location: {
+                center: [ 125.5092, -8.99334],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+        
+        
+        
+        
+        
+        
+        
+          
+        {
+            id: 'BAUCAU',
+            alignment: 'left',
+            hidden: false,
+            title: 'Distrito BAUCAU [av_attachment_soon]',
+            image: 'images/BAUCAU.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+
+            location: {
+                center: [ 126.45249, -8.46211],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+        
+        
+        
+        
+        
+        
+        
+        
+           
+        {
+            id: 'BOBONARO',
+            alignment: 'right',
+            hidden: false,
+            title: 'Distrito BOBONARO [av_attachment_soon]',
+            image: 'images/BOBONARO.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+
+            location: {
+                center: [ 125.21991, -8.99192],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        {
+            id: 'COVALIMA',
+            alignment: 'left',
+            hidden: false,
+            title: 'Distrito COVALIMA [av_attachment_soon]',
+            image: 'images/COVALIMA.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+
+            location: {
+                center: [ 125.2516, -9.30941],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+          
+        
+        
+        
+        
+        
+        
+        
+         {
+            id: 'DILI',
+            alignment: 'right',
+            hidden: false,
+            title: 'Distrito DILI [av_attachment_soon]',
+            image: 'images/DILI.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+
+            location: {
+                center: [ 125.56309, -8.55515],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+          
+        
+        
+        
+        
+        
+        
+         {
+            id: 'ERMERA',
+            alignment: 'left',
+            hidden: false,
+            title: 'Distrito ERMERA [av_attachment_soon]',
+            image: 'images/ERMERA.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+
+            location: {
+                center: [ 125.3966, -8.75157],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+          
+        
+        
+        
+        
+        
+        
+        {
+            id: 'LAUTEM',
+            alignment: 'right',
+            hidden: false,
+            title: 'Distrito LAUTEM [av_attachment_soon]',
+            image: 'images/LAUTEM.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+            
+            location: {
+                center: [ 126.905678, -8.362989],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+        
+        
+        
+        
+        
+        
+         
+        {
+            id: 'LIQUICA',
+            alignment: 'left',
+            hidden: false,
+            title: 'Distrito LIQUICA [av_attachment_soon]',
+            image: 'images/LIQUICA.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+            
+            
+            location: {
+                center: [ 125.350311, -8.583031],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+      
+      
+        
+        
+        
+        
+        
+        {
+            id: 'MANATUTO',
+            alignment: 'right',
+            hidden: false,
+            title: 'Distrito MANATUTO [av_attachment_soon]',
+            image: 'images/MANATUTO.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+           
+           
+            location: {
+                center: [ 126.017395, -8.515897],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+        
+        
+        
+        
+        
+        
+        
+        
+        {
+            id: 'MANUFAHI',
+            alignment: 'left',
+            hidden: false,
+            title: 'Distrito MANUFAHI [av_attachment_soon]',
+            image: 'images/MANUFAHI.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+           
+           
+            location: {
+                center: [ 125.649910, -9.005694],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+      
+      
+      
+      
+      
+      
+        {
+            id: 'OECUSSE',
+            alignment: 'right',
+            hidden: false,
+            title: 'Distrito OECUSSE [av_attachment_soon]',
+            image: 'images/OECUSSE.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+           
+           
+            location: {
+                center: [ 124.37139, -9.20102],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+      
+        
+        
+        
+        
+        
+        
+        {
+            id: 'VIQUEQUE',
+            alignment: 'left',
+            hidden: false,
+            title: 'Distrito VIQUEQUE [av_attachment_soon]',
+            image: 'images/VIQUEQUE.png',
+            description: 'Our mission is to help facilitate the newly independent East-Timor in accessing the voices and the key events in its past and fostering an understanding of its unique cultural heritage; to help unite and secure the identity of the country and its people; and to contribute to the forging of a new democratic nation. To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.',
+           
+           
+            location: {
+                center: [ 126.3657600, -8.8672900],  
+                zoom: 15.00,
+                pitch: 9.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []                
+        },
+      
+      
+        
+        
+    ]
+};
