5b9db6217ca54e17e907caf797ea624c01f6b27c
ict-gis-mapping repositories project, design and prototype <24618720+timorleste@users.noreply.github.com>
2021-01-04 01:06:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ var config = {
     showMarkers: true,
     markerColor: '#3FB1CE',
     theme: 'light',
-    use3dTerrain: false,
+    use3dTerrain: true,
     title: 'CAMSTL',
     subtitle: 'Centro Audiovisual Max Stahl Timor Leste',
     byline: 'The importance of The Archive derives from the crucial role that audiovisual material played in the East Timorese struggle for self-determination and human rights. That struggle was captured in the faces and the voices of this very dedicated, resilient and persistent people by International film-maker Max Stahl on over 6000 hours of film and audio recording.',
-    footer: '&copy; 2020  [ Rua Formosa No.19. Vera Cruz, Suco Colmera, Rai Nain. Dili, Timor-Leste ]',
+    footer: '&copy; 2020--Rua Formosa No.19. Vera Cruz, Suco Colmera, Rai Nain. Dili, Timor-Leste',
            
     chapters: [
         {
@@ -17,7 +17,7 @@ var config = {
             hidden: false,
             title: 'Server Back Office Host Specification',
             image: 'images/camstl.png',
-            description: 'In Timor-Leste, Regionally and Internationally</p>  <iframe width="100%" height="260" src="https://www.youtube.com/embed/gj3kDaKqMoI?controls=0&amp;start=207" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
+            description: 'In Timor-Leste, Regionally and Internationally</p>  <iframe width="100%" height="240" src="https://www.youtube.com/embed/gj3kDaKqMoI?controls=0&amp;start=207" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
             
             location: {
                 center: [125.57726, -8.55524],  
@@ -69,7 +69,7 @@ var config = {
             title: 'Distrito AILEU',
             image: 'images/AILEU.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                      
+
             location: {
                 center: [ 125.56693, -8.71643],  
                 zoom: 15.00,
@@ -92,7 +92,7 @@ var config = {
             title: 'Distrito AINARO',
             image: 'images/AINARO.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                                  
+
             location: {
                 center: [ 125.5092, -8.99334],  
                 zoom: 15.00,
@@ -106,7 +106,7 @@ var config = {
             onChapterExit: []                
         },
         
-            
+           
           
         {
             id: 'BAUCAU',
@@ -115,7 +115,7 @@ var config = {
             title: 'Distrito BAUCAU',
             image: 'images/BAUCAU.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                                   
+
             location: {
                 center: [ 126.45249, -8.46211],  
                 zoom: 15.00,
@@ -138,7 +138,7 @@ var config = {
             title: 'Distrito BOBONARO',
             image: 'images/BOBONARO.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                                   
+
             location: {
                 center: [ 125.21991, -8.99192],  
                 zoom: 15.00,
@@ -161,7 +161,7 @@ var config = {
             title: 'Distrito COVALIMA',
             image: 'images/COVALIMA.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                                   
+
             location: {
                 center: [ 125.2516, -9.30941],  
                 zoom: 15.00,
@@ -184,7 +184,7 @@ var config = {
             title: 'Distrito DILI',
             image: 'images/DILI.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                                
+
             location: {
                 center: [ 125.56309, -8.55515],  
                 zoom: 15.00,
@@ -207,7 +207,7 @@ var config = {
             title: 'Distrito ERMERA',
             image: 'images/ERMERA.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                             
+
             location: {
                 center: [ 125.3966, -8.75157],  
                 zoom: 15.00,
@@ -230,7 +230,7 @@ var config = {
             title: 'Distrito LAUTEM',
             image: 'images/LAUTEM.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                                   
+
             location: {
                 center: [ 126.905678, -8.362989],  
                 zoom: 15.00,
@@ -253,7 +253,7 @@ var config = {
             title: 'Distrito LIQUICA',
             image: 'images/LIQUICA.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                               
+
             location: {
                 center: [ 125.350311, -8.583031],  
                 zoom: 15.00,
@@ -276,7 +276,7 @@ var config = {
             title: 'Distrito MANATUTO',
             image: 'images/MANATUTO.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-         
+
             location: {
                 center: [ 126.017395, -8.515897],  
                 zoom: 15.00,
@@ -299,7 +299,7 @@ var config = {
             title: 'Distrito MANUFAHI',
             image: 'images/MANUFAHI.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                                          
+
             location: {
                 center: [ 125.649910, -9.005694],  
                 zoom: 15.00,
@@ -322,7 +322,7 @@ var config = {
             title: 'Distrito OECUSSE',
             image: 'images/OECUSSE.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-                        
+
             location: {
                 center: [ 124.37139, -9.20102],  
                 zoom: 15.00,
@@ -345,7 +345,7 @@ var config = {
             title: 'Distrito VIQUEQUE',
             image: 'images/VIQUEQUE.png',
             description: 'To ensure the survival of unique resources which contain and communicate the experience and the story of Timor-Leste and to place these at the service of peace and understanding, in Timor-Leste, Regionally and Internationally.</p>  <video width="100%" height="225" src="images/av-test.mp4" controls></video>',
-            
+
             location: {
                 center: [ 126.3657600, -8.8672900],  
                 zoom: 15.00,
